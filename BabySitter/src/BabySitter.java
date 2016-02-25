311a92076dd012c25bf1ad20602d32707ed6571f
Manoj <pasupuleti.3@osu.edu>
2016-02-25 15:34:08 -0500

    Declared "final" variables midnight,after_midnight_pay,start_time_to_bed_time_pay,bed_time_to_midnight

--- BabySitter/src/BabySitter.java
+++ BabySitter/src/BabySitter.java
@@ -1,6 +1,12 @@
 
 public class BabySitter {
 
+	private static final int MIDNIGHT = 24;
+	private static final int AFTER_MIDNIGHT_PAY = 16;
+	private static final int START_TIME_TO_BED_TIME_PAY = 12;
+	private static final int BED_TIME_TO_MIDNIGHT_PAY = 8;
+
+	
 	public String paytotal(Integer starttime, Integer bedtime, Integer endtime) {
 		
 		if (starttime<17)
@@ -14,27 +20,27 @@ public class BabySitter {
 
 	public int beforebedtime(Integer starttime, Integer bedtime, Integer endtime) {
 		
-		if((starttime <= bedtime) && (starttime <= 24))
+		if((starttime <= bedtime) && (starttime <= MIDNIGHT))
 		{
-			return (Math.min(Math.min(bedtime, 24),endtime)-starttime)*12;
+			return (Math.min(Math.min(bedtime, MIDNIGHT),endtime)-starttime)*START_TIME_TO_BED_TIME_PAY;
 		}
 		return 0;
 	}
 
 	public int betweenBedtimeAndMidnight(Integer starttime, Integer bedtime, Integer endtime) {
-		if((endtime>=bedtime)&&(bedtime<=24))
+		if((endtime>=bedtime)&&(bedtime<=MIDNIGHT))
 		{
 		
-		return (Math.min(24,endtime) - Math.max(starttime,bedtime))*8;
+		return (Math.min(MIDNIGHT,endtime) - Math.max(starttime,bedtime))*BED_TIME_TO_MIDNIGHT_PAY;
 		}
 		return 0;
 	}
 
 	public int afterMidnight(Integer starttime, Integer bedtime, Integer endtime) {
-		if((endtime >= 24))
+		if((endtime >= MIDNIGHT))
 		{
 			Integer pay=0;
-			pay= (endtime-Math.max(starttime, 24))*16;
+			pay= (endtime-Math.max(starttime, MIDNIGHT))*AFTER_MIDNIGHT_PAY;
 			return pay;
 		}
 		return 0;

77df1b49fb33631d6e58e933f65897f7b5c03218
Manoj <pasupuleti.3@osu.edu>
2016-02-25 15:44:10 -0500

    Added a HashMap which converts hours after Midnight like 1am,2am,3am,4am to 25,26,27,28

--- BabySitter/src/BabySitter.java
+++ BabySitter/src/BabySitter.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class BabySitter {
 
@@ -5,9 +8,23 @@ public class BabySitter {
 	private static final int AFTER_MIDNIGHT_PAY = 16;
 	private static final int START_TIME_TO_BED_TIME_PAY = 12;
 	private static final int BED_TIME_TO_MIDNIGHT_PAY = 8;
-
 	
+	private static Map<Integer, Integer> mapA;
+	static {
+		mapA = new HashMap<Integer, Integer>();
+		mapA.put(1, 25);
+		mapA.put(2, 26);
+		mapA.put(3, 27);
+		mapA.put(4, 28);
+	}
+	
+		
 	public String paytotal(Integer starttime, Integer bedtime, Integer endtime) {
+		
+		int start = (mapA.get(starttime) == null) ? starttime : mapA.get(starttime);
+		int bed = (mapA.get(bedtime) == null) ? bedtime : mapA.get(bedtime);
+		int end = (mapA.get(endtime) == null) ? endtime : mapA.get(endtime);
+
 		
 		Integer total=0;
 		if (starttime<17)
@@ -16,9 +33,9 @@ public class BabySitter {
 		if(endtime>28)
 		return"EnterEndTimeLessThan4am";
 		
-		total+=beforebedtime( starttime,  bedtime,  endtime);
-		total+=betweenBedtimeAndMidnight( starttime,  bedtime,  endtime);
-		total+=afterMidnight( starttime,  bedtime,  endtime);
+		total+=beforebedtime( start,  bed,  end);
+		total+=betweenBedtimeAndMidnight( start,  bed,  end);
+		total+=afterMidnight( start,  bed,  end);
 		
 		return total.toString();
 		

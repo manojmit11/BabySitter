5d3baee7452350eb75e49b31ff54f4b137495770
Manoj <pasupuleti.3@osu.edu>
2016-02-25 15:11:13 -0500

    BabySitter Refractor

--- BabySitter/src/BabySitterTest.java
+++ BabySitter/src/BabySitterTest.java
@@ -1,14 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class BabySitterTest {
 
+	BabySitter babySitter;
+	
+	@Before
+	public void setUp(){
+		babySitter=new BabySitter();
+	}
+	
 	@Test
 	public void ifStartTimeisBeforefiveReturnEnterStartTimegreaterThan5pm() {
 		BabySitter babySitter=new BabySitter();
 		assertEquals("EnterStartTimegreaterThan5pm", babySitter.paytotal(16,20,25));
 	}
+	
+	
 
 }
